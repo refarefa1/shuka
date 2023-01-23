89bcef500fb3da0f946f1a2937b9423dd29f8b52
Refael Abramov <refaelavramov@gmail.com>
2023-01-23 12:10:27 +0200

    upload to render

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv"
 import express from "express"
 import * as bodyParser from "body-parser"
 import cors from 'cors'
+import path from 'path'
 import { fruitRouter } from "./routes/fruit.routes"
 
 const app = express()
@@ -10,16 +11,17 @@ dotenv.config()
 app.use(bodyParser.json())
 
 const corsOptions = {
-    origin: ['https://shuka-fruits.netlify.app', 'http://127.0.0.1:5173', 'http://localhost:5173'],
+    origin: ['https://shuka.onrender.com/', 'http://127.0.0.1:5173', 'http://localhost:5173'],
     credentials: true
 }
 app.use(cors(corsOptions))
 
 app.use("/api/fruit", fruitRouter)
 
-app.get("/", function (req, res) {
-    res.send("Hello Express");
-});
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
 
 app.listen(process.env.PORT, () => {
     console.log("Node server started running", process.env.PORT)
--- dist/app.js
+++ dist/app.js
@@ -30,18 +30,19 @@ const dotenv = __importStar(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const bodyParser = __importStar(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const fruit_routes_1 = require("./routes/fruit.routes");
 const app = (0, express_1.default)();
 dotenv.config();
 app.use(bodyParser.json());
 const corsOptions = {
-    origin: ['https://shuka-fruits.netlify.app', 'http://127.0.0.1:5173', 'http://localhost:5173'],
+    origin: ['https://shuka.onrender.com/', 'http://127.0.0.1:5173', 'http://localhost:5173'],
     credentials: true
 };
 app.use((0, cors_1.default)(corsOptions));
 app.use("/api/fruit", fruit_routes_1.fruitRouter);
-app.get("/", function (req, res) {
-    res.send("Hello Express");
+app.get('/**', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(process.env.PORT, () => {
     console.log("Node server started running", process.env.PORT);
